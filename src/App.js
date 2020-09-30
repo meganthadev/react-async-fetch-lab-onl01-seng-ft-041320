e24ed0f54913e928a0452c5bf9bbe4b4bbedea9b
Megan Horsfield <meganthadev@gmail.com>
2020-09-30 05:43:54 +0000

    Automatically backed up by Learn

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import React from "react";
 
 class App extends React.Component {
@@ -26,3 +27,22 @@ class App extends React.Component {
 }
 
 export default App;
+=======
+import 'React', {Component} from 'react';
+
+
+class App extends Component {
+  
+  
+  state = {
+    
+  }
+  
+  componentDidMount {
+    fetch(http://api.open-notify.org/astros.json)
+    .then(res => res.json())
+    .then(json => this.setState({data: json}))
+  }
+  
+}
+>>>>>>> 059fd580d57ccad241e45ac5e687354b33bb568a
